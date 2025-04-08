0babe8cdbdc34de8770bef7cbbe003e304060252
vladimir <vovka1998@gmail.com>
2025-04-08 14:23:54 +0300

    fixed validate datetime

--- schemas/reservations.py
+++ schemas/reservations.py
@@ -1,6 +1,6 @@
 import datetime
 
-from pydantic import BaseModel, Field, FutureDatetime
+from pydantic import BaseModel, Field, FutureDatetime, field_validator
 from schemas.tables import InfoTable, IDTable
 
 class BaseReservation(BaseModel):
@@ -27,6 +27,10 @@ class InfoReservation(IDReservation):
     duration_minutes: int = Field(description="Продолжительность брони в минутах", examples=[30, 60])
     table: InfoTable = Field(description="Столик брони")
 
+    @field_validator("reservation_time", mode="after")
+    def validate_reservation_time(cls, value: datetime.datetime) -> str:
+        return value.strftime("%d.%m.%Y %H:%M")
+
 class CreateReservation(BaseReservation):
     customer_name: str = Field(description="Имя клиента", examples=["Владимир"])
     reservation_time: datetime.datetime = Field(
@@ -35,3 +39,7 @@ class CreateReservation(BaseReservation):
     )
     duration_minutes: int = Field(description="Продолжительность брони в минутах", examples=[30, 60])
     table_id: int = Field(description="ID столика брони")
+
+    @field_validator("reservation_time", mode="before")
+    def validate_reservation_time(cls, value: str) -> datetime.datetime:
+        return datetime.datetime.strptime(value, "%d.%m.%Y %H:%M")
