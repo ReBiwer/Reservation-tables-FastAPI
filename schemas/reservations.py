53016d5eb307c5eb5f8c680b6859023dcbdf7bef
vladimir <vovka1998@gmail.com>
2025-04-08 14:10:59 +0300

    change datetime format

--- schemas/reservations.py
+++ schemas/reservations.py
@@ -1,21 +1,28 @@
 import datetime
 
-from pydantic import BaseModel, Field, ConfigDict
+from pydantic import BaseModel, Field, FutureDatetime
 from schemas.tables import InfoTable, IDTable
 
 class BaseReservation(BaseModel):
-    model_config = ConfigDict(from_attributes=True)
+
+    class Config:
+        from_attributes = True
+        json_encoders = {
+            datetime: lambda dt: dt.strftime("%d-%m-%Y %H:%M")
+        }
 
 
 class IDReservation(BaseReservation):
     id: int = Field(description="ID брони")
 
+example_datetime = datetime.datetime.now() + datetime.timedelta(days=1)
+formated_example_datetime = example_datetime.strftime("%d.%m.%Y %H:%M")
 
 class InfoReservation(IDReservation):
     customer_name: str = Field(description="Имя клиента", examples=["Владимир"])
-    reservation_time: datetime.datetime = Field(
+    reservation_time: FutureDatetime = Field(
         description="Время брони",
-        examples=[datetime.datetime.now() + datetime.timedelta(days=1)]
+        examples=[formated_example_datetime],
     )
     duration_minutes: int = Field(description="Продолжительность брони в минутах", examples=[30, 60])
     table: InfoTable = Field(description="Столик брони")
@@ -24,7 +31,7 @@ class CreateReservation(BaseReservation):
     customer_name: str = Field(description="Имя клиента", examples=["Владимир"])
     reservation_time: datetime.datetime = Field(
         description="Время брони",
-        examples=[datetime.datetime.now() + datetime.timedelta(days=1)]
+        examples=[formated_example_datetime]
     )
     duration_minutes: int = Field(description="Продолжительность брони в минутах", examples=[30, 60])
     table_id: int = Field(description="ID столика брони")
