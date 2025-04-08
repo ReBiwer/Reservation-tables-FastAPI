e65d5bc6aa67b44bb494fdfe4fc039833631ec4e
vladimir <vovka1998@gmail.com>
2025-04-08 10:17:54 +0300

    add router for Table

---
+++ routers/table_router.py
@@ -0,0 +1,39 @@
+from fastapi import APIRouter, Depends
+from fastapi import status
+from fastapi.responses import JSONResponse
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from dao.dao import TableDAO
+from dependencies.dao_dep import get_session_with_commit, get_session_without_commit
+from schemas.tables import IDTable, InfoTable, CreateTable
+
+router = APIRouter()
+
+@router.get('/')
+async def get_tables(
+        session: AsyncSession = Depends(get_session_without_commit)
+) -> list[InfoTable]:
+    table_dao = TableDAO(session)
+    tables = await table_dao.find_all()
+    return [InfoTable.model_validate(table) for table in tables]
+
+
+@router.post('/')
+async def create_table(
+        table: CreateTable,
+        session: AsyncSession = Depends(get_session_with_commit)
+) -> InfoTable:
+    table_dao = TableDAO(session)
+    table = await table_dao.add(table)
+    return InfoTable.model_validate(table)
+
+
+@router.delete('/')
+async def delete_table(
+        table: IDTable,
+        session: AsyncSession = Depends(get_session_with_commit)
+) -> JSONResponse:
+    table_dao = TableDAO(session)
+    await table_dao.delete(table)
+    return JSONResponse(content={"message": "Стол удален"},
+                        status_code=status.HTTP_204_NO_CONTENT)
