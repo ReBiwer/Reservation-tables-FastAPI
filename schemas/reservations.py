3277da54442b562be6f028abe56b1a1837db5fa3
vladimir <vovka1998@gmail.com>
2025-04-09 14:34:57 +0300

    fix schema

--- schemas/reservations.py
+++ schemas/reservations.py
@@ -6,6 +6,7 @@ from schemas.tables import InfoTable, IDTable
 class BaseReservation(BaseModel):
     model_config = ConfigDict(
         from_attributes = True,
+        json_encoders={datetime.datetime: lambda dt: dt.strftime("%d-%m-%Y %H:%M")}
     )
 
 
@@ -17,16 +18,13 @@ formated_example_datetime = example_datetime.strftime("%d.%m.%Y %H:%M")
 
 class InfoReservation(IDReservation):
     customer_name: str = Field(description="Имя клиента", examples=["Владимир"])
-    reservation_time: FutureDatetime = Field(
+    reservation_time: datetime.datetime = Field(
         description="Время брони",
         examples=[formated_example_datetime],
     )
     duration_minutes: int = Field(description="Продолжительность брони в минутах", examples=[30, 60])
     table: InfoTable = Field(description="Столик брони")
 
-    @field_validator("reservation_time", mode="after")
-    def validate_reservation_time(cls, value: datetime.datetime) -> str:
-        return value.strftime("%d.%m.%Y %H:%M")
 
 class CreateReservation(BaseReservation):
     customer_name: str = Field(description="Имя клиента", examples=["Владимир"])
@@ -36,7 +34,3 @@ class CreateReservation(BaseReservation):
     )
     duration_minutes: int = Field(description="Продолжительность брони в минутах", examples=[30, 60])
     table_id: int = Field(description="ID столика брони")
-
-    @field_validator("reservation_time", mode="before")
-    def validate_reservation_time(cls, value: str) -> datetime.datetime:
-        return datetime.datetime.strptime(value, "%d.%m.%Y %H:%M")
