0315cc259ad21d76c3ceac0cb9e70a9fd1b0f130
vladimir <vovka1998@gmail.com>
2025-04-09 15:17:42 +0300

    add cascade delete reservation

--- models.py
+++ models.py
@@ -10,7 +10,11 @@ class Table(Base):
     seats: Mapped[int]
     location: Mapped[str]
 
-    reservations: Mapped[list["Reservation"]] = relationship(back_populates="table")
+    reservations: Mapped[list["Reservation"]] = relationship(
+        back_populates="table",
+        cascade="all, delete-orphan",
+        passive_deletes=True
+    )
 
     def __repr__(self):
         return f"{self.__class__.__name__}(id={self.id}, name={self.name})"
@@ -21,7 +25,7 @@ class Reservation(Base):
     reservation_time: Mapped[datetime.datetime]
     duration_minutes: Mapped[int]
 
-    table_id: Mapped[int] = mapped_column(ForeignKey('tables.id'))
+    table_id: Mapped[int] = mapped_column(ForeignKey('tables.id', ondelete="CASCADE"))
     table: Mapped["Table"] = relationship("Table", back_populates="reservations", lazy="joined")
 
     def __repr__(self):
