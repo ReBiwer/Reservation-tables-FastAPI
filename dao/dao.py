ccd33b70c7abae876de1562d4c20c1830d383f41
vladimir <vovka1998@gmail.com>
2025-04-09 15:18:02 +0300

    fix method for check instance

--- dao/dao.py
+++ dao/dao.py
@@ -1,5 +1,5 @@
 from loguru import logger
-from sqlalchemy import exists
+from sqlalchemy import exists, select
 from sqlalchemy.exc import SQLAlchemyError
 
 from exceptions.reservations import CreateReservationException
@@ -15,21 +15,22 @@ class ReservationDAO(BaseDAO):
     model = Reservation
 
     async def check_instance(self, values: CreateReservation):
-        return await self._session.execute(
-            exists().
-            where(
-                self.model.reservation_time == values.reservation_time,
-                self.model.table_id == values.table_id,
-            ).
-            scalar()
+        result = await self._session.execute(
+            select(
+                exists().where(
+                    (self.model.reservation_time == values.reservation_time) &
+                    (self.model.table_id == values.table_id)
+                )
+            )
         )
+        return result.scalar()
 
     async def add(self, values: CreateReservation):
         values_dict = values.model_dump(exclude_unset=True)
         logger.info(f"Добавление записи {self.model.__name__} с параметрами: {values_dict}")
         try:
             new_instance = self.model(**values_dict)
-            if not self.check_instance(values):
+            if not await self.check_instance(values):
                 self._session.add(new_instance)
                 logger.info(f"Запись {self.model.__name__} успешно добавлена.")
                 await self._session.commit()
