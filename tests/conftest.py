ea1f4d069a4600ff8143097d12d77adad36fe21f
vladimir <vovka1998@gmail.com>
2025-04-10 12:15:49 +0300

    add tests for reservations router

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,12 @@
+import datetime
+
 import pytest_asyncio
 from httpx import AsyncClient, ASGITransport
 from unittest.mock import AsyncMock
 from fastapi.testclient import TestClient
 
 from schemas.tables import InfoTable
+from schemas.reservations import InfoReservation
 from main import app
 
 
@@ -17,9 +20,14 @@ async def async_client():
 
 
 @pytest_asyncio.fixture(scope="function")
-async def mock_table_dao_add(mocker):
+async def mock_session(mocker):
     session = mocker.MagicMock()
     session.commit = mocker.AsyncMock()
+    yield session
+
+
+@pytest_asyncio.fixture(scope="function")
+async def mock_table_dao_add(mocker, mock_session):
     data = InfoTable(id=1, name="Test table", seats=3, location="test location")
     mock_data = data.model_dump()
     mocker.patch(
@@ -29,27 +37,24 @@ async def mock_table_dao_add(mocker):
     )
     mocker.patch(
         "routers.table_router.get_session_with_commit",
-        return_value=session
+        return_value=mock_session
     )
 
+
 @pytest_asyncio.fixture(scope="function")
-async def mock_table_dao_delete(mocker):
-    session = mocker.MagicMock()
-    session.commit = mocker.AsyncMock()
-    mocker.patch(
-        "dao.dao.TableDAO.delete",
+async def mock_table_dao_delete(mocker, mock_session):
+        mocker.patch(
+            "dao.dao.TableDAO.delete",
         new_callable=AsyncMock,
         return_value=1
-    )
-    mocker.patch(
-        "routers.table_router.get_session_with_commit",
-        return_value=session
-    )
+        )
+        mocker.patch(
+            "routers.table_router.get_session_with_commit",
+            return_value=mock_session
+        )
 
 @pytest_asyncio.fixture(scope="function")
-async def mock_table_dao_delete_not_found(mocker):
-    session = mocker.MagicMock()
-    session.commit = mocker.AsyncMock()
+async def mock_table_dao_delete_not_found(mocker, mock_session):
     mocker.patch(
         "dao.dao.TableDAO.delete",
         new_callable=AsyncMock,
@@ -57,5 +62,97 @@ async def mock_table_dao_delete_not_found(mocker):
     )
     mocker.patch(
         "routers.table_router.get_session_with_commit",
-        return_value=session
+        return_value=mock_session
     )
+
+@pytest_asyncio.fixture(scope="function")
+async def mock_reservation_dao_find_all(mocker, mock_session):
+    data = [
+      {
+        "id": 12,
+        "customer_name": "Владимир",
+        "reservation_time": datetime.datetime(year=2025, month=4, day=20, hour=16, minute=30),
+        "duration_minutes": 30,
+        "table": {
+          "id": 6,
+          "name": "Table 3",
+          "seats": 2,
+          "location": "зал у окна"
+        }
+      },
+      {
+        "id": 13,
+        "customer_name": "Владимир",
+        "reservation_time": datetime.datetime(year=2025, month=4, day=12, hour=16, minute=30),
+        "duration_minutes": 30,
+        "table": {
+          "id": 6,
+          "name": "Table 3",
+          "seats": 2,
+          "location": "зал у окна"
+        }
+      }
+    ]
+    mocker.patch(
+        "dao.dao.ReservationDAO.find_all",
+        new_callable=AsyncMock,
+        return_value=data
+    )
+    mocker.patch(
+        "routers.reservation_router.get_session_without_commit",
+        return_value=mock_session
+    )
+
+
+@pytest_asyncio.fixture(scope="function")
+async def mock_reservation_dao_add(mocker, mock_session):
+    data = (
+        InfoReservation(
+            id=1,
+            customer_name="Владимир",
+            reservation_time=datetime.datetime(year=2025, month=4, day=12, hour=16, minute=30),
+            duration_minutes=30,
+            table=InfoTable(
+                id=1,
+                name="Test table",
+                seats=3,
+                location="test location"
+            )
+        )
+    )
+    mock_data = data.model_dump()
+    mocker.patch(
+        "dao.dao.ReservationDAO.add",
+        new_callable=AsyncMock,
+        return_value=mock_data
+    )
+    mocker.patch(
+        "routers.table_router.get_session_with_commit",
+        return_value=mock_session
+    )
+
+@pytest_asyncio.fixture(scope="function")
+async def mock_reservation_dao_delete(mocker, mock_session):
+        mocker.patch(
+            "dao.dao.ReservationDAO.delete",
+        new_callable=AsyncMock,
+        return_value=1
+        )
+        mocker.patch(
+            "routers.table_router.get_session_with_commit",
+            return_value=mock_session
+        )
+
+@pytest_asyncio.fixture(scope="function")
+async def mock_reservation_dao_delete_not_found(mocker, mock_session):
+    mocker.patch(
+        "dao.dao.ReservationDAO.delete",
+        new_callable=AsyncMock,
+        return_value=0
+    )
+    mocker.patch(
+        "routers.table_router.get_session_with_commit",
+        return_value=mock_session
+    )
+
+
